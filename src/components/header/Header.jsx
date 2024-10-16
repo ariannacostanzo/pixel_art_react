acad5cb256a43ab18d6c82bddeff8f0aacac3b99
ariannacostanzo <costanzo.arianna@outlook.it>
2024-10-16 15:03:18 +0200

    creating header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,29 @@
+import { useState } from 'react'
+import './header.scss'
+
 const Header = () => {
+
+    const [currentColor, setCurrentColor] = useState('#FFFFFF');
+
+    const choseColor = (e) => {
+        setCurrentColor(e.target.value)
+    }
      return (
-     <>
-       <header>header</header>
-     </>
-     )
+       <>
+         <header>
+           <h1 className="text-3xl">Pixel Art</h1>
+           <div>
+             <label htmlFor="colorPalette" className='mr-2'> Scegli il colore</label>
+
+             <input 
+             id='colorPalette'
+               type="color"
+               onChange={choseColor}
+               value={currentColor}
+             />
+           </div>
+         </header>
+       </>
+     );
 }
 export default Header;

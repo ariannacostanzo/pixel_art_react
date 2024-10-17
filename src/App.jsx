b755507bee061a41936c96660936a99889a8591f
ariannacostanzo <costanzo.arianna@outlook.it>
2024-10-17 21:21:01 +0200

    adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Grid from "./components/grid/Grid";
 import Header from "./components/header/Header";
 import { GridProvider } from "./providers/GridContext.jsx";
 import { ColorProvider } from "./providers/ColorContext.jsx";
+import Footer from "./components/footer/Footer.jsx";
 function App() {
   
 
@@ -12,6 +13,7 @@ function App() {
           <Header></Header>
 
           <Grid></Grid>
+          <Footer></Footer>
         </ColorProvider>
       </GridProvider>
     </>
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import './footer.scss'
+
+const Footer = () => {
+     return (
+       <>
+         <footer>
+           Made with &#10084; by{" "}
+           <a href="https://arianna-costanzo-portfolio.netlify.app">
+             Arianna Costanzo
+           </a>
+         </footer>
+       </>
+     );
+}
+export default Footer;<footer>Made with </footer>
